7c3ff3051b5053fcd110b026fea51c792ada6e46
joe <joe.ulica@gmail.com>
2023-06-20 11:24:32 -0400

    add show and route and template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 class ArticlesController < ApplicationController
-  def get_article
-    @article = Article.find(params[:id])
+  def get_article(id)
+    @article = Article.find(id)
   end
   
   def index
     @articles = Article.all
     render template: "articles/index"
+  end
+
+  def show
+    get_article(params[:id])
+    render template: "articles/show"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/articles" => "articles#index"
+  get "/articles/:id" => "articles#show"
 end
